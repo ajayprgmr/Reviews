1c3d782b6b6cb82e231e37612ffacc393880120c
Ajay Kushwaha <ak383834@gmail.com>
2023-12-10 13:12:16 +0530

    almost completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,37 @@
+import React, { useState } from 'react'
+import people from './assets/data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+import '../src/App.css'
+
 function App() {
-  return <h2>Reviews Start up</h2>
+  const [index, setIndex] = useState(0)
+  const { name, job, image, text } = people[index]
+  const handlePreBtn = () => {
+    setIndex((index-1+people.length)%people.length)
+  }
+  const handleNextBtn = () => {
+    setIndex((index+1)%people.length)
+  }
+  return (
+    <article>
+      <h2 className='title'>Reviews</h2>
+      <div className='review'>
+        <div className='img-container'>
+          <span className='quote-icon'>
+            <FaQuoteRight />
+          </span>
+          <img src={image} alt={name} className='person-image' />
+        </div>
+        <h4 className='author'>{name}</h4>
+        <h6 className='job'>{job}</h6>
+        <p className='info'>{text}</p>
+        <div className='btn'>
+          <FaChevronLeft className='pre-btn' onClick={handlePreBtn} />
+          <FaChevronRight className='next-btn' onClick={handleNextBtn} />
+        </div>
+      </div>
+    </article>
+  )
 }
+
 export default App
